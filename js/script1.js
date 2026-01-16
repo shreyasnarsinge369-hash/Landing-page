f23df336ca65b5dd7b7e679c8296e9e23bb7ff4c
shreyasnarsinge369-hash <shreyasnarsinge@gmail.com>
2026-01-16 19:55:24 +0530

    Updated script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,1 +1,17 @@
-console.log("Landing page loaded");
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(0)";
+    }
+  });
+});
+
+reveals.forEach(el => {
+  el.style.opacity = 0;
+  el.style.transform = "translateY(40px)";
+  observer.observe(el);
+});
+
